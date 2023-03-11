fc3a544b595cd2565e7a5fd6165ecbd209cde1f5
Tuấn David <43137730+VitStudio@users.noreply.github.com>
2023-03-11 15:05:40 +0700

    Update sngto.py
    Added int(n**0.5) + 1 as the upper limit for the loop, which is more efficient than checking all numbers up to n.
    
    Signed-off-by: Tuấn David <43137730+VitStudio@users.noreply.github.com>

--- sngtolib/sngto.py
+++ sngtolib/sngto.py
@@ -1,18 +1,19 @@
 dem = 0 # biến toàn cục
 def check_prime_number(n):
-    #flag = 0 => không phải số nguyên tố
-    #flag = 1 => số nguyên tố
-    flag = 1;
-    if (n <2):
+    # flag = 0 => không phải số nguyên tố
+    # flag = 1 => số nguyên tố
+    flag = 1
+    if n < 2:
         flag = 0
-        return flag  #Số nhỏ hơn 2 không phải số nguyên tố => trả về 0
+        return flag  # Số nhỏ hơn 2 không phải số nguyên tố => trả về 0
     
-    #Sử dụng vòng lặp while để kiểm tra có tồn tại ước số nào khác không
-    for i in range(2, n):
+    # Sử dụng vòng lặp for để kiểm tra có tồn tại ước số nào khác không
+    for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             flag = 0
-            break #Chỉ cần tìm thấy 1 ước số là đủ và thoát vòng lặp
+            break  # Chỉ cần tìm thấy 1 ước số là đủ và thoát vòng lặp
     return flag
+
 
 """n = int(input(">> nhap mot so n: "))
 for i in range(n):
