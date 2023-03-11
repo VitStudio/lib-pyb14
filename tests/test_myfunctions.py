6a9d3c6c672e9244c5e7a42678a5734e4792e5ed
Tuấn David <43137730+VitStudio@users.noreply.github.com>
2023-03-11 15:06:15 +0700

    Update test_myfunctions.py
    Signed-off-by: Tuấn David <43137730+VitStudio@users.noreply.github.com>

--- tests/test_myfunctions.py
+++ tests/test_myfunctions.py
@@ -1,6 +1,25 @@
 from sngtolib import sngto
-n = 2
 
-def test_prime():
-    assert sngto.check_prime_number(n) == 2
-print("test_prime() passed")
+def test_check_prime_number():
+    assert check_prime_number(1) == 0
+    assert check_prime_number(2) == 1
+    assert check_prime_number(3) == 1
+    assert check_prime_number(4) == 0
+    assert check_prime_number(5) == 1
+    assert check_prime_number(6) == 0
+    assert check_prime_number(7) == 1
+    assert check_prime_number(8) == 0
+    assert check_prime_number(9) == 0
+    assert check_prime_number(10) == 0
+    assert check_prime_number(11) == 1
+    assert check_prime_number(12) == 0
+    assert check_prime_number(13) == 1
+    assert check_prime_number(14) == 0
+    assert check_prime_number(15) == 0
+    assert check_prime_number(16) == 0
+    assert check_prime_number(17) == 1
+    assert check_prime_number(18) == 0
+    assert check_prime_number(19) == 1
+    assert check_prime_number(20) == 0
+
+test_check_prime_number()
